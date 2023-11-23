6af171e490e236f6bcc0bd33ef5655ff942e8bcc
Mateus Stangherlin <mateus_stangherlin@hotmail.com>
2023-11-23 18:02:13 -0300

    Nova rota'

--- src/routes/provas_routes.py
+++ src/routes/provas_routes.py
@@ -1,14 +1,24 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from src.models.provas_model import Provas
 from src.config.database import get_session
+from sqlalchemy import select, and_
 
 provas_router = APIRouter(prefix="/provas")
 
 @provas_router.post("")
 def cria_prova(prova: Provas):
     with get_session() as session:
+        
+        statement = select(Provas).where(
+            and_(Provas.descricao == prova.descricao, Provas.data_prova == prova.data_prova)
+        )
+        prova_existente = session.exec(statement).first()
+        if prova_existente:
+            raise HTTPException(status_code=400, detail="Prova já cadastrada.")
+        
         session.add(prova)
         session.commit()
         session.refresh(prova)
+        
         return prova
--- src/routes/resultados_routes.py
+++ src/routes/resultados_routes.py
@@ -11,18 +11,15 @@ resultados_router = APIRouter(prefix="/resultados")
 @resultados_router.post("")
 def cria_prova(resultado: Resultados):
     with get_session() as session:
-        # Calcular a nota baseado nas questòes da prova
-
-        # Buscar a prova do banco de dados
+        
         statement = select(Provas).where(Provas.id == resultado.prova_id)
         prova = session.exec(statement).first()
-
-        # se tiver prova, vai calcular a nota
-
-        
+       
         resultado.nota = 10
 
         session.add(resultado)
         session.commit()
         session.refresh(resultado)
         return resultado
+
+
